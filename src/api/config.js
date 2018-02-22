7c4afc5d4997351fc347344eca9110417752222c
Edgardo Hernández <ehernandez@colegium.com>
2018-02-22 15:36:24 -0300

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,12 @@
+// Here are the details of your new API account.
+//
+//   Application name	platzimusic
+// API key	24e8f1ec3ea6b82be29aef32b57cd0d4
+// Shared secret	e10c0380fa7bef06d8c0f159b863b514
+// Registered to	edgherna
+export default {
+  appName: 'platzimusic',
+  apiKey: '24e8f1ec3ea6b82be29aef32b57cd0d4',
+  secret: 'e10c0380fa7bef06d8c0f159b863b514',
+  registeredTo: 'edgherna'
+}
